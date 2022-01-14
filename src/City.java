52d501056db9d7d2b7de72dd38dc148dddd6744c
Tobias Schnabel <70516553+CTS98@users.noreply.github.com>
2022-01-14 16:07:06 +0100

    added constructors, getters, setters, distance calculation instance method for city objects, added file input

--- src/City.java
+++ src/City.java
@@ -1,4 +1,57 @@
 public class City {
+
+    //declare immutabe city attributes
+    private final String name;
+    private final double numInhab;
+    private final double latCoord;
+    private final double longCoord;
+
+    //constructor
+    public City(String cityName, int numberInhab, double latitude, double longitude) {
+        this.name = cityName;
+        this.numInhab = numberInhab;
+        this.latCoord = latitude;
+        this.longCoord = longitude;
+    }
+
+
+    public double distance (City that) {
+        //method applies Haversine's formula to calculate as-the-crow-flies distance between cities
+/*        a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
+          c = 2 ⋅ atan2( √a, √(1−a) )
+          d = R ⋅ c
+        where	φ is latitude, λ is longitude, R is earth’s radius (mean radius = 6,371km);
+        note that angles need to be in radians to pass to trig functions*/
+        final double radius = 6371e3; //earth radius in m
+        //convert to radians
+        final double phi_one = this.getLatCoord() * Math.PI / 180;
+        final double phi_two = that.getLatCoord() * Math.PI / 180;
+        final double delta_phi = (this.latCoord - that.latCoord) * Math.PI / 180;
+        final double delta_lambda = (this.longCoord - that.longCoord) * Math.PI / 180;
+
+        final double a = Math.sin(delta_phi / 2) * Math.sin(delta_lambda / 2) + Math.cos(phi_one) * Math.cos(phi_two) * Math.sin(delta_lambda / 2) * Math.sin(delta_lambda / 2);
+        final double c =  2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        return radius * c;
+    }
+
+    //getters for attributes
+    public String getName() {
+        return name;
+    }
+
+    public double getNumInhab() {
+        return numInhab;
+    }
+
+    public double getLatCoord() {
+        return latCoord;
+    }
+
+    public double getLongCoord() {
+        return longCoord;
+    }
+
+
 }
 //constructor needs coordinates, name, population
 //(instance) methods for distance, adjacency?
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        City[] cityList = null;
+
+        String filename = "personList.txt";
+        try{
+            cityList = importList(filename);
+            System.out.println("Import successful.");
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static City[] importList(String filename)
+
+        throws java.io.FileNotFoundException{
+        File file = new File(filename);
+        Scanner input = new Scanner (file);
+        int n = input.nextInt();
+
+        City[] list = new City[n];
+
+        for(int i=0; i<n; i++){
+            String cityName = input.next();
+            int numberInhab = input.nextInt();
+            double latitude = input.nextDouble();
+            double longitude = input.nextDouble();
+
+            list[i] = new City(cityName, numberInhab, latitude, longitude);
+        }
+
+        input.close();
+
+        return list;
+    }
 }
 
 /* needs methods for
-1. Data import
-2. instantiating Cities as objects using City class
+1. Data import #done
+2. instantiating Cities as objects using City class #done
 3. iterative elimination method
 4. local search method
 
